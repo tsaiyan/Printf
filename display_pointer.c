b0535220dac86c1c78f1e639f244099f691d75fe
tsaiyan <tsaiyan21@yandex.ru>
2020-12-11 10:41:17 +0300

    Norme: display_unsigned.c

--- display_pointer.c
+++ display_pointer.c
@@ -12,11 +12,15 @@
 
 #include "ft_printf.h"
 
-void	ft_putnbr_p(unsigned long n, t_struct *box)
+/*
+** special putnbr version for pointers
+*/
+
+void		ft_putnbr_p(unsigned long n, t_struct *box)
 {
-	char *array;
-	char result_array[ft_rank_count(n, 16) + 1];
-	size_t len;
+	char	*array;
+	char	result_array[ft_rank_count(n, 16) + 1];
+	size_t	len;
 
 	len = ft_rank_count(n, 16);
 	array = "0123456789abcdef";
@@ -34,71 +38,81 @@ void	ft_putnbr_p(unsigned long n, t_struct *box)
 	if (ft_crutch(n, box))
 		return ;
 	while (result_array[len])
-	ft_putchar(result_array[len++], box);
+		ft_putchar(result_array[len++], box);
 }
 
-void	display_pointer(t_struct *box)
-{
-	unsigned long n;
-	int precision;
-	int wight;
+/*
+** if format has align flag
+*/
 
-	n = va_arg(box->ap, unsigned long);
-/* меняет знак, если n < 0 */
-	precision = (int)(box->precision - ft_rank_count(n, 16));
-	wight = box->wight - ((precision > 0) ? precision : 0) - (int)ft_rank_count(n, 16)\
-															- 2;
-	(ft_crutch(n, box))? wight++ : 0;
-/* если есть выравнивание */
-	if (box->align)
+static void	ft_align(unsigned long n, t_struct *box)
+{
+	if (box->ox)
 	{
-		if (box->ox)
-		{
-			ft_putstr(box->ox, box);
-			wight--;
-		}
-		while (precision-- > 0)
-			ft_putchar(48, box);
-		(ft_crutch(n, box))? 0 : ft_putnbr_p(n, box);
-		wight += ((box->ox) ? 1 : 0);
-		while (wight-- > 0)
-			ft_putchar(32, box);
+		ft_putstr(box->ox, box);
+		box->new_wight--;
 	}
-/* если нет выравнивания */
+	while (box->new_precision-- > 0)
+		ft_putchar(48, box);
+	(ft_crutch(n, box)) ? 0 : ft_putnbr_p(n, box);
+	box->new_wight += ((box->ox) ? 1 : 0);
+	while (box->new_wight-- > 0)
+		ft_putchar(32, box);
+}
+
+/*
+** if format has zero flag
+*/
+
+static void	ft_zero(unsigned long n, t_struct *box)
+{
+	if (box->precision != -1)
+	{
+		while (box->new_wight-- > 0)
+			ft_putchar(32, box);
+		if (box->ox)
+			ft_putstr(box->ox, box);
+	}
 	else
 	{
-		/* если есть zero */
+		if (box->ox)
+			ft_putstr(box->ox, box);
+		while (box->new_wight-- > 0)
+			ft_putchar(48, box);
+	}
+	while (box->new_precision-- > 0)
+		ft_putchar(48, box);
+	ft_putnbr_p(n, box);
+}
+
+/*
+** 	main function for display pointers
+*/
+
+void		display_pointer(t_struct *box)
+{
+	unsigned long n;
+
+	n = va_arg(box->ap, unsigned long);
+	box->new_precision = (int)(box->precision - ft_rank_count(n, 16));
+	box->new_wight = box->wight - ((box->new_precision > 0) ? \
+	box->new_precision : 0) - (int)ft_rank_count(n, 16) - 2;
+	(ft_crutch(n, box)) ? box->new_wight++ : 0;
+	if (box->align)
+		ft_align(n, box);
+	else
+	{
 		if (box->zero)
-		{
-			if (box->precision != -1)
-			{
-				while (wight-- > 0)
-					ft_putchar(32, box);
-				if (box->ox)
-					ft_putstr(box->ox, box);
-			}
-			else
-			{
-				if (box->ox)
-					ft_putstr(box->ox, box);
-				while (wight-- > 0)
-					ft_putchar(48, box);
-			}
-			while (precision-- > 0)
-				ft_putchar(48, box);
-			ft_putnbr_p(n, box);
-		}
-		/* если нет zero */
+			ft_zero(n, box);
 		else
 		{
-			while (wight-- > 0)
+			while (box->new_wight-- > 0)
 				ft_putchar(32, box);
-			/* есть есть знак */
 			if (box->precision)
 				ft_putstr(box->ox, box);
-			while (precision-- > 0)
+			while (box->new_precision-- > 0)
 				ft_putchar(48, box);
-			(ft_crutch(n, box))? ft_putnbr_p(0, box) : ft_putnbr_p(n, box);
+			(ft_crutch(n, box)) ? ft_putnbr_p(0, box) : ft_putnbr_p(n, box);
 		}
 	}
 	ft_putnull(box);
--- display_unsigned.c
+++ display_unsigned.c
@@ -12,68 +12,65 @@
 
 #include "ft_printf.h"
 
-void	display_unsigned(t_struct *box)
+static void	ft_align(unsigned n, t_struct *box)
 {
-	unsigned n;
-	int precision;
-	int wight;
-
-	n = va_arg(box->ap, unsigned);
-/* меняет знак, если n < 0 */
-	precision = (int)(box->precision - ft_rank_count(n, 10));
-	wight = box->wight - ((precision > 0) ? precision : 0) - (int)ft_rank_count(n, 10)\
-															- ((box->znak) ? 1 : 0);
-	(!n && !box->precision && box->point)? wight++ : 0;
-/* если есть выравнивание */
-	if (box->align)
+	if (box->znak)
 	{
-		if (box->znak)
-		{
-			ft_putchar(box->znak, box);
-			wight--;
-		}
-		while (precision-- > 0)
-			ft_putchar(48, box);
-		(!n && !box->precision && box->point)? box->retlen-- : ft_putnbr(n);
-		wight += ((box->znak) ? 1 : 0);
-		while (wight-- > 0)
-			ft_putchar(32, box);
+		ft_putchar(box->znak, box);
+		box->new_wight--;
 	}
-/* если нет выравнивания */
+	while (box->new_precision-- > 0)
+		ft_putchar(48, box);
+	(!n && !box->precision && box->point) ? box->retlen-- : ft_putnbr(n);
+	box->new_wight += ((box->znak) ? 1 : 0);
+	while (box->new_wight-- > 0)
+		ft_putchar(32, box);
+}
+
+static void	ft_zero(unsigned n, t_struct *box)
+{
+	if (box->precision >= 0)
+	{
+		while (box->new_wight-- > 0)
+			ft_putchar(32, box);
+		if (box->znak)
+			ft_putchar(box->znak, box);
+	}
 	else
 	{
-		/* если есть zero */
+		if (box->znak)
+			ft_putchar(box->znak, box);
+		while (box->new_wight-- > 0)
+			ft_putchar(48, box);
+	}
+	while (box->new_precision-- > 0)
+		ft_putchar(48, box);
+	(!n && box->point && !box->precision) ? box->retlen-- : ft_putnbr((int)n);
+}
+
+void		display_unsigned(t_struct *box)
+{
+	unsigned n;
+
+	n = va_arg(box->ap, unsigned);
+	box->new_precision = (int)(box->precision - ft_rank_count(n, 10));
+	box->new_wight = box->wight - ((box->new_precision > 0) ? \
+	box->new_precision : 0) - (int)ft_rank_count(n, 10) - ((box->znak) ? 1 : 0);
+	(!n && !box->precision && box->point) ? box->new_wight++ : 0;
+	if (box->align)
+		ft_align(n, box);
+	else
+	{
 		if (box->zero)
-		{
-			if (box->precision >= 0)
-			{
-				while (wight-- > 0)
-					ft_putchar(32, box);
-				if (box->znak)
-				ft_putchar(box->znak, box);
-			}
-			else
-			{
-				if (box->znak)
-					ft_putchar(box->znak, box);
-				while (wight-- > 0)
-					ft_putchar(48, box);
-			}
-			while (precision-- > 0)
-				ft_putchar(48, box);
-			(!n && box->point && !box->precision) ? box->retlen-- : ft_putnbr((int)n);
-		}
-		/* если нет zero */
+			ft_zero(n, box);
 		else
 		{
-			while (wight-- > 0)
+			while (box->new_wight-- > 0)
 				ft_putchar(32, box);
-			/* есть есть знак */
-			if (box->znak)
-				ft_putchar(box->znak, box);
-			while (precision-- > 0)
+			while (box->new_precision-- > 0)
 				ft_putchar(48, box);
-			(!n && !box->precision && box->point)? box->retlen-- : ft_putnbr(n);
+			(!n && !box->precision && box->point) \
+								? box->retlen-- : ft_putnbr(n);
 		}
 	}
 	box->retlen += ft_rank_count(n, 10);
